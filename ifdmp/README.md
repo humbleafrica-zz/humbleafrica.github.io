b84f74aa354e9f2f7b965e5b34c1ffdca8993155
humbleafrica <kofiasiedu@hotmail.co.uk>
2018-12-05 11:38:13 +0000

    file upload

---
+++ ifdmp/README.md
@@ -0,0 +1,82 @@
+     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
+    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
+    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
+    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
+     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
+    ----------------------------------------------------------------- 
+
+Interactive Front End Development Milestone Project (IFEDMP)
+
+Hi there! Welcome to my code institute IFEDMP
+
+In this project I have choosen the option to create a single page Google Maps API based website.
+
+The criteria of the project is shown below:
+
+1. Create a site that calls on the Google Maps API and/or the Google Places API (or similar) to allow users to search for their next holiday destination. 
+2. You'll want to help your users:
+        
+      - Select a destination city
+      - Find tourist attractions
+      - Find accommodation
+      - Find bars and restaurants
+
+3. Provide search results in a manner that is visually appealing for your user (by drawing on the skills you have learned in User-Centric Frontend Development)
+
+
+I am building the page specifically aim at people who may be visiting my home country of Ghana and would want to search for restaurants to dine in.
+
+My webpage offers the ability to search for destinations, attractions, accomodation and many others from the provided interface. 
+The interface has anumber of selections that can be used to determine the search results. I added an extra button to offer another service but 
+deemed it as an extra clutter so I blanked out the html code so it doesnt show on the interface.
+
+Features:
+
+    A search bar with a place holder and an autocomplete field - This text box makes use of the Google Maps and Places APIs autocomplet feaures. The instruction
+    to interact in through a place holder to keep the design simple, tidy but effective.
+
+    The second feature of my page is the number of radio buttons - The options to choose between four states of results rendering worldwide.
+
+    The third feature is the used of a checkbox - The checkbox focuses users search within the viewport area. 
+    In this case it would search for locations in the southern parts of Ghana, Ivory Cost, Togo, Benin and southwest Nigeria.
+
+    The final feature of the page is the Reset button - The reset button allows the user to clear the textbox, uncheck the checkbox and return the rendering option
+    to the 'All' radio button.
+
+
+Features Left to Implement
+
+    I thought it to be a good idea to have the user enter a near location when not wanting to use location services. However I felt the option already existed in the API
+    and therefore appeared as if reinventing the wheel
+
+Technologies Used
+
+    I used a little of what I know as a coder and a little of what I have learnt since joining the code institute. The coding technologies used in the project are HTML5, CSS3 
+    and Javascript.
+    
+    The HTML5 was used for the structuring. 
+    No inline CSS was sued so a separate CSS file was created for the styling and
+    Javascript is used to manipulate the Maps interface.
+
+Testing
+    
+    Not a lot of testing was required in this project as most of the code came from credible external . Nonetheless the extra added features were tested and inspected to ensure
+    there were no errors appearing when actions were taken.
+    
+
+The page also works on all device types without difficulties. This capability was tested within Firefox's Developers Edition browser's virtual device renderer.
+
+I visited the google places webpage, looked at existing examples and made changes to suit my needs, there were some copy and pasting before modification
+
+the following turtorials were referenced:
+
+https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+https://developers.google.com/maps/documentation/javascript/importing_data
+
+
+link for project is https://ifed-milestone-project-humbleafrika.c9users.io/
+link for github page https://github.com/humbleafrica/Pages
+
+I wanted to add an option for users to enter A near by destination but the prompt request for the entrt wasn't working.
+
+I believe this project satisfies the conditions of the guidelines and therefore is presentable
---
+++ ifdmp/assets/script.js
@@ -0,0 +1,136 @@
+//OWN FUNCTIONS
+
+//reset function
+function reset() {
+    var center = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 7, lng: 0.0 },
+        zoom: 8
+    });
+
+    document.getElementById('pac-input').value = '';
+    document.getElementById('use-strict-bounds').checked = false;
+    document.getElementById('changetype-all').checked = true;
+    document.getElementById('map').center;
+}
+
+
+/*
+//this location function
+function thisloc() {
+    //request access to location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        });
+    }
+
+    //error msg if any
+    if (navigator.geolocation) {
+
+    }
+    else {
+        alert('Sorry your browser doesn\'t support the Geolocation API');
+    }
+
+    var initialLocation = navigator.geolocation;
+    var center = new google.maps.Map(document.getElementById('map'), {
+        center: initialLocation,
+        zoom: 1
+    });
+
+    document.getElementById('pac-input').value = '';
+    document.getElementById('map').setCenter(initialLocation);
+
+}
+
+*/
+//COPIED AND MODIFIED FUNCTIONS 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 7, lng: 0.0 },
+        zoom: 8
+    });
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    var types = document.getElementById('type-selector');
+    var strictBounds = document.getElementById('strict-bounds-selector');
+
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        }
+        else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17); // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, marker);
+    });
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+        var radioButton = document.getElementById(id);
+        radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+        });
+    }
+
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-address', ['address']);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+
+    document.getElementById('use-strict-bounds')
+        .addEventListener('click', function() {
+            console.log('Checkbox clicked! New state=' + this.checked);
+            autocomplete.setOptions({ strictBounds: this.checked });
+        });
+}
